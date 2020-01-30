bb04294155a33cd54e4db01764650e39d7e1d404
vineettt <vineetyadav.hacker@gmail.com>
2020-01-30 20:04:17 +0530

    update

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true});
+
+// console.log(mongoose.connection.readyState);
+mongoose.connection.on('connected', ()=>{
+    console.log(`Connected to database ${config.database}`);
+});
+
+mongoose.connection.on('error', ()=>{
+    console.log(`Database error ${err}`);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid ENdpoint');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/meanauth',
+    secret: '1ueSyCp4nqv2dxb99OxU3vphThQnJjIltRKQ0ER0yCE'
+}
